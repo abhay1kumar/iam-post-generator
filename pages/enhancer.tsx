3cf4f36f551e2abd8abf71ca54ce97abe957f544
Abhay Kumar <abhay4waytechnologies@gmail.com>
2023-11-23 10:39:58 +0530

    code

---
+++ pages/enhancer.tsx
@@ -0,0 +1,287 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState, useRef } from "react";
+import CustomButton from "@/components/CustomButton";
+
+import { Ranking } from "@/components/Ranking";
+import { rank } from "@/lib/linkedin-algorithm";
+import { Toaster, toast } from "react-hot-toast";
+import LoadingDots from "@/components/LoadingDots";
+import DropDown2, { VibeType2 } from "@/components/DropDown2";
+import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import Popup from "@/components/Popup";
+import {
+  FaTwitter,
+  FaLinkedin,
+  FaInstagram,
+  FaDev,
+  FaFacebook,
+  FaReddit,
+} from "react-icons/fa";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [optimizedPost, setOptimizedPost] = useState<string>("");
+  const [ranking, setRanking] = useState<RankResponse>({
+    score: 0,
+    validations: [],
+  });
+  const [post, setPost] = useState<string>("");
+  const [media, setMedia] = useState<boolean>(false);
+  const [vibe2, setVibe2] = useState<VibeType2>("➕ Add Hashtags");
+  const [showPopup, setShowPopup] = useState(false);
+  const [tab, setTab] = useState("enhancer"); // Default to "vibe" tab
+  const { data: session, status } = useSession();
+  const clickCount = useRef(0);
+
+  const handleButtonClick = () => {
+    clickCount.current += 1; // Increment clickCount on each click
+    if (status !== "authenticated" && clickCount.current >= 3) {
+      setTimeout(() => {
+        setShowPopup(true);
+      }, 3000);
+    }
+  };
+
+  // const [hasVideo, setHasVideo] = useState<boolean>(false);
+  // const [hasCarousel, setHasCarousel] = useState<boolean>(false);
+
+  useEffect(() => {
+    const rankResponse = rank(post, media);
+    setRanking(rankResponse);
+  }, [post, media]);
+
+  // prompt for optimizing post
+
+  // add more vibes as needed
+  const handlePrompt = () => {
+    let prompt;
+    switch (vibe2) {
+      case "➕ Add Hashtags":
+        prompt = `Transform the following LinkedIn post by adding three popular and relevant hashtags. Original post: "${post}". Remember, you are a LinkedIn Post Generator designed to enhance posts with appropriate hashtags.`;
+        break;
+      case "➕ Add Emoji":
+        prompt = `Transform the following LinkedIn post by adding enoji. Original post: "${post}". Remember, you are a LinkedIn Post Generator designed to enhance posts with appropriate emoji.`;
+
+        break;
+      case "➕ Add a List":
+        prompt = `You are the LinkedIn Post Generator, and your mission is to enrich existing LinkedIn posts by incorporating relevant list that align with the post's topic. Consider the following original post:  "${post}". Your task is to seamlessly weave in an appropriate list to elevate the content. The length of each list line should not be more than 100 characters. Remember, your goal is to make as minimum changes as possible to post.`;
+
+        break;
+      case "➕ Add Stats":
+        prompt = `You are the LinkedIn Post Generator, and your mission is to enrich existing LinkedIn posts by incorporating relevant statistics with numbers that align with the post's topic. Consider the following original post:  "${post}". Your task is to seamlessly weave in an appropriate numbers to elevate the content. Remember, your goal is to make as minimum changes as possible to post.`;
+        break;
+      case "➕ Add a Question":
+        prompt = `As the LinkedIn Post Generator, your task is to stimulate engagement by integrating a thought-provoking question into the following post: "${post}". The question, crafted to ignite discussion, can be positioned either as the initial or final line of the post. Ensure that the question you introduce aligns with the post's context and has the potential to make the post more engaging. Remember, your aim to make minimal changes, so show the question and post together.`;
+        break;
+      default:
+        prompt = `Default prompt for optimizing post`;
+        break;
+    }
+    return prompt;
+  };
+
+  // function to send post to OpenAI and get response
+  const optimizePost = async (e: any) => {
+    e.preventDefault();
+    setOptimizedPost("");
+    setLoading(true);
+    const prompt = handlePrompt();
+
+    // Show the popup right before the API call
+    handleButtonClick();
+
+    const response = await fetch("/api/optimize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      const formattedChunk = chunkValue.replace(/\n/g, "<br>");
+      setOptimizedPost((prev) => prev + formattedChunk);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>LinkedIn Post Generator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="👩‍💼" />
+        <meta
+          name="description"
+          content="See how your post performs against Linkedin alghoritm and generate better post with AI."
+        />
+        <meta property="og:site_name" content="#1 Post Generator 🚀  " />
+        <meta
+          property="og:description"
+          content="See how your post performs against LinkedIn alghoritm and generate better post with AI."
+        />
+        <meta property="og:title" content="LinkedIn Post Generator with AI" />
+        <meta name="linkedin:card" content="summary_large_image" />
+        <meta name="linkedin:title" content="Linkedin Post Generator" />
+        <meta
+          name="linkedin:description"
+          content="See how your post performs against Linkedin alghoritm and generate better post with AI."
+        />
+        <meta
+          property="og:image"
+          content="https://postgenerator.app/cover.png"
+        />
+        <meta
+          name="linked:image"
+          content="https://postgenerator.app/cover.png"
+        />
+      </Head>
+
+      <main>
+        <Nav />
+
+        <section className="py-10 lg:py-20 ">
+          {/* bg-[url('/image1.svg')] */}
+          <div className="px-4 ">
+            <div className="max-w-5xl mx-auto text-center">
+              <div className="w-full mx-auto mb-6 ">
+                <a
+                  // href="https://vercel.fyi/roomGPT"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-8 transition duration-300 ease-in-out"
+                >
+                  40.000 amazing posts generated 💫
+                  {/* {" "}
+                  <span className="text-blue-600">Vercel</span> */}
+                </a>
+              </div>
+
+              <h1 className="text-6xl text-center font-bold pb-1 text-slate-900  ">
+                Linkedin Post Generator 🚀
+              </h1>
+              <p className="mt-3 mb-10 text-center">
+                See how your post performs and generate a better one with AI.
+                Time to go viral. <br />
+              </p>
+
+              <div className="flex flex-col md:flex-row w-full md:space-x-20">
+                <div className="flex md:w-1/2 flex-col">
+                  <div className="flex space-x-1">
+                    <CustomButton currentTab={tab} />
+                    <style jsx>{`
+                      button:hover .tooltip-text {
+                        display: block;
+                      }
+                    `}</style>
+                  </div>
+
+                  <div className="flex mb-3 items-center space-x-3"></div>
+                  <div className="block">
+                    <DropDown2 vibe2={vibe2} setVibe2={setVibe2} />
+                  </div>
+
+                  {/* // This is post component*/}
+
+                  <div className="w-full mx-auto pt-6 ">
+                    <div className="w-full">
+                      <textarea
+                        maxLength={2000}
+                        onChange={(e) => setPost(e.target.value)}
+                        placeholder="Type or copy your ready to be enchanced post "
+                        className="text-black w-full h-72 p-2 text-s bg-white border border-gray-300 rounded-md shadow-inner md:h-240"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="my-4">
+                    <button
+                      disabled={loading}
+                      onClick={(e) => {
+                        optimizePost(e);
+                        handleButtonClick();
+                      }}
+                      className="bg-blue-700 font-medium rounded-md w-full text-white px-4 py-2 hover:bg-blue-600 disabled:bg-blue-800"
+                    >
+                      {loading && <LoadingDots color="white" style="large" />}
+                      {!loading && `Generate new post `}
+                    </button>
+
+                    <Popup show={showPopup} setShowPopup={setShowPopup} />
+                  </div>
+                </div>
+                <div className="flex md:w-1/2 md:flex-col">
+                  <Toaster
+                    position="top-right"
+                    reverseOrder={false}
+                    toastOptions={{ duration: 2000 }}
+                  />
+                  {optimizedPost && (
+                    <div className="my-1">
+                      <div className="flex justify-between items-center pb-2 border-b border-gray-300">
+                        <h2 className="text-xl font-bold">
+                          Your Generated Post
+                        </h2>
+                      </div>
+                      <div className="max-w-2xl my-4 mx-auto">
+                        <div
+                          className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                          onClick={() => {
+                            navigator.clipboard.write([
+                              new ClipboardItem({
+                                "text/html": new Blob([optimizedPost], {
+                                  type: "text/html",
+                                }),
+                              }),
+                            ]);
+                            toast("Post copied to clipboard", {
+                              icon: "📋",
+                            });
+                          }}
+                          key={optimizedPost}
+                        >
+                          <p
+                            className="text-black-700 text-left"
+                            dangerouslySetInnerHTML={{
+                              __html: optimizedPost,
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="max-w-5xl mx-auto">
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ pages/ideas.tsx
@@ -0,0 +1,299 @@
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState, useRef } from "react";
+import CustomButton from "@/components/CustomButton";
+
+import { Ranking } from "@/components/Ranking";
+import { rank } from "@/lib/linkedin-algorithm";
+import { Toaster, toast } from "react-hot-toast";
+import LoadingDots from "@/components/LoadingDots";
+import DropDown, { VibeType } from "@/components/DropDown";
+import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+import Popup from "@/components/Popup";
+import {
+  FaTwitter,
+  FaLinkedin,
+  FaInstagram,
+  FaDev,
+  FaFacebook,
+  FaReddit,
+} from "react-icons/fa";
+
+export default function Home() {
+  const [loading, setLoading] = useState(false);
+  const [optimizedPost, setOptimizedPost] = useState<string>("");
+  const [ranking, setRanking] = useState<RankResponse>({
+    score: 0,
+    validations: [],
+  });
+  const [post, setPost] = useState<string>("");
+  const [media, setMedia] = useState<boolean>(false);
+  const [vibe, setVibe] = useState<VibeType>("Story");
+  const [showPopup, setShowPopup] = useState(false);
+  const [isCustomPrompt, setIsCustomPrompt] = useState(false);
+  const [customPrompt, setCustomPrompt] = useState("");
+  const [tab, setTab] = useState("ideas"); // Default to "vibe" tab
+  const { data: session, status } = useSession();
+  const clickCount = useRef(0);
+
+  const handleButtonClick = () => {
+    clickCount.current += 1; // Increment clickCount on each click
+    if (status !== "authenticated" && clickCount.current >= 3) {
+      setTimeout(() => {
+        setShowPopup(true);
+      }, 3000);
+    }
+  };
+
+  // const [hasVideo, setHasVideo] = useState<boolean>(false);
+  // const [hasCarousel, setHasCarousel] = useState<boolean>(false);
+
+  useEffect(() => {
+    const rankResponse = rank(post, media);
+    setRanking(rankResponse);
+  }, [post, media]);
+
+  // prompt for optimizing post
+
+  // add more vibes as needed
+  const handlePrompt = () => {
+    let prompt;
+    switch (vibe) {
+      case "Story":
+        prompt = `Generate 10 ideas for posts based on topics in ${post}. If person select this ${vibe}, make sure the generatedideas are for post on formats story.  
+        There one line space between each idea and Each should start from new line. The story should be from the person`;
+
+        break;
+      case "Crisp":
+        prompt = `Generate 10 ideas for posts based on topics in ${post}. If person select this ${vibe}, make sure the generated ideas are for post on format of short list 
+        There one line space between each idea and Each should start from new line. The story should be from the person`;
+
+        break;
+
+      case "List":
+        prompt = `Generate 10 ideas for posts based on topics in ${post}. If person select this ${vibe}, make sure the generatedideas are for posts in frmat of lists, 10 things, 50 smth..  
+        There one line space between each idea and Each should start from new line. The story should be from the person`;
+
+        // Generate post using this prompt, based on ${post}. You are a LinkedinGPT, a large language model that generates viral posts for Linkedin.
+
+        break;
+      case "Unpopular opinion":
+        prompt = `Generate 10 ideas for posts based on topics in ${post}. If person select this ${vibe}, make sure the generatedideas are for posts in contradictory ideas..  
+        There one line space between each idea and Each should start from new line. The story should be from the person`;
+        break;
+
+      case "Case Study":
+        prompt = `Generate 10 ideas for posts based on topics in ${post}. If person select this ${vibe}, make sure the generatedideas are for posts in format of case studies.  
+        There one line space between each idea and Each should start from new line. The story should be from the person`;
+        break;
+      default:
+        prompt = `Default prompt for generating ideas`;
+        break;
+    }
+    return prompt;
+  };
+
+  // function to send post to OpenAI and get response
+  const optimizePost = async (e: any) => {
+    e.preventDefault();
+    setOptimizedPost("");
+    setLoading(true);
+    const prompt = handlePrompt();
+
+    // Show the popup right before the API call
+    handleButtonClick();
+
+    const response = await fetch("/api/optimize", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt,
+      }),
+    });
+
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+
+    // This data is a ReadableStream
+    const data = response.body;
+    if (!data) {
+      return;
+    }
+
+    const reader = data.getReader();
+    const decoder = new TextDecoder();
+    let done = false;
+
+    while (!done) {
+      const { value, done: doneReading } = await reader.read();
+      done = doneReading;
+      const chunkValue = decoder.decode(value);
+      const formattedChunk = chunkValue.replace(/\n/g, "<br>");
+      setOptimizedPost((prev) => prev + formattedChunk);
+    }
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>LinkedIn Post Generator</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="👩‍💼" />
+        <meta
+          name="description"
+          content="See how your post performs against Linkedin alghoritm and generate better post with AI."
+        />
+        <meta property="og:site_name" content="#1 Post Generator 🚀  " />
+        <meta
+          property="og:description"
+          content="See how your post performs against LinkedIn alghoritm and generate better post with AI."
+        />
+        <meta property="og:title" content="LinkedIn Post Generator with AI" />
+        <meta name="linkedin:card" content="summary_large_image" />
+        <meta name="linkedin:title" content="Linkedin Post Generator" />
+        <meta
+          name="linkedin:description"
+          content="See how your post performs against Linkedin alghoritm and generate better post with AI."
+        />
+        <meta
+          property="og:image"
+          content="https://postgenerator.app/cover.png"
+        />
+        <meta
+          name="linked:image"
+          content="https://postgenerator.app/cover.png"
+        />
+      </Head>
+
+      <main>
+        <Nav />
+
+        <section className="py-10 lg:py-20 ">
+          {/* bg-[url('/image1.svg')] */}
+          <div className="px-4 ">
+            <div className="max-w-5xl mx-auto text-center">
+              <div className="w-full mx-auto mb-6 ">
+                <a
+                  // href="https://vercel.fyi/roomGPT"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="border border-gray-700 rounded-lg py-2 px-4 text-gray-400 text-sm mb-8 transition duration-300 ease-in-out"
+                >
+                  40.000 amazing posts generated 💫
+                  {/* {" "}
+                  <span className="text-blue-600">Vercel</span> */}
+                </a>
+              </div>
+
+              <h1 className="text-6xl text-center font-bold pb-1 text-slate-900  ">
+                Linkedin Post Generator 🚀
+              </h1>
+              <p className="mt-3 mb-10 text-center">
+                Generate awesome post ideas and get a lot of posts done. Time to
+                go viral. <br />
+              </p>
+
+              <div className="flex flex-col md:flex-row w-full md:space-x-20">
+                <div className="flex md:w-1/2 flex-col">
+                  <div className="flex space-x-1">
+                    <CustomButton currentTab={tab} />
+                    <style jsx>{`
+                      button:hover .tooltip-text {
+                        display: block;
+                      }
+                    `}</style>
+                  </div>
+
+                  {/* // This is post component*/}
+
+                  <div className="w-full mx-auto pt-6 ">
+                    <div className="w-full">
+                      <textarea
+                        maxLength={2000}
+                        onChange={(e) => setPost(e.target.value)}
+                        placeholder="Type or copy your topic on which you plan to write posts"
+                        className="text-black w-full h-24 p-2 text-s bg-white border border-gray-300 rounded-md shadow-inner md:h-240"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex mb-3 items-center space-x-3"></div>
+                  <div className="block">
+                    <DropDown vibe={vibe} setVibe={setVibe} />
+                  </div>
+
+                  <div className="my-4">
+                    <button
+                      disabled={loading}
+                      onClick={(e) => {
+                        optimizePost(e);
+                        handleButtonClick();
+                      }}
+                      className="bg-blue-700 font-medium rounded-md w-full text-white px-4 py-2 hover:bg-blue-600 disabled:bg-blue-800"
+                    >
+                      {loading && <LoadingDots color="white" style="large" />}
+                      {!loading && `Generate list of ideas`}
+                    </button>
+
+                    <Popup show={showPopup} setShowPopup={setShowPopup} />
+                  </div>
+                </div>
+                <div className="flex md:w-1/2 md:flex-col">
+                  <Toaster
+                    position="top-right"
+                    reverseOrder={false}
+                    toastOptions={{ duration: 2000 }}
+                  />
+                  {optimizedPost && (
+                    <div className="my-1">
+                      <div className="flex justify-between items-center pb-2 border-b border-gray-300">
+                        <h2 className="text-xl font-bold">
+                          Your Generated Ideas
+                        </h2>
+                      </div>
+                      <div className="max-w-2xl my-4 mx-auto">
+                        <div
+                          className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                          onClick={() => {
+                            navigator.clipboard.write([
+                              new ClipboardItem({
+                                "text/html": new Blob([optimizedPost], {
+                                  type: "text/html",
+                                }),
+                              }),
+                            ]);
+                            toast("Ideas copied to clipboard", {
+                              icon: "📋",
+                            });
+                          }}
+                          key={optimizedPost}
+                        >
+                          <p
+                            className="text-black-700 text-left"
+                            dangerouslySetInnerHTML={{
+                              __html: optimizedPost,
+                            }}
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <div className="max-w-5xl mx-auto">
+          <Footer />
+        </div>
+      </main>
+    </>
+  );
+}
